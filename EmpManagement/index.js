d1c8076483d9cca25782e9a8448df6812689c381
jyotikumari994 <jyotikumaripatwa94@gmail.com>
2024-04-28 18:34:31 +0530

    ss

---
+++ EmpManagement/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded"); function(){
+    const containerr=document.getElementById("container");
+    const container_1=document.getElementById("container1");
+    const container_2=document.getElementById("container2");
+    
+    .fetch(":https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees")
+    .then( response =>response.json)
+        .then(employee=>
+        )}
+    if(sort===asc){
+        
+    }
+     
+    
