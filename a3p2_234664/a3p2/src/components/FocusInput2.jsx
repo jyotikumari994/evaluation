6f27077d50a81bbb6dd52084ab54bb3ff0a6b55c
jyotikumari994 <jyotikumaripatwa94@gmail.com>
2024-05-25 19:00:52 +0530

    a3p2

---
+++ a3p2_234664/a3p2/src/components/FocusInput2.jsx
@@ -0,0 +1,25 @@
+// src/components/FocusInput2.jsx
+
+import { useRef, useEffect } from "react";
+
+function FocusInput2() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Focus me on component mount"
+      />
+    </div>
+  );
+}
+
+export default FocusInput2;
